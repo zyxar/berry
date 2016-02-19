46541bb48e62299aa444505e2acc2eef6e49e9e7
Markus Tzoe <chou.marcus@gmail.com>
2016-02-19 11:18:29 +0800

    add doc: i2c device lists

--- bus/i2c.go
+++ bus/i2c.go
@@ -1,4 +1,66 @@
 // Package bus provides low level control over the linux buses.
+/*
+I²C Device List
+http://www.coris.org.uk/jdc/Notes/i2c-devices.html
+
+                          Device                Address range
+
+Bridge/Expander
+                          SUNW,i2c-imax         0x09, 0x0b, 0x0c, 0x12, 0x30
+Clock Generator
+                          ICS951601             0x6e
+                          ICS9FG108             0x6e
+Controller
+                          PCF8584               [Programmable]
+                          Sun JBus I2C          [N/A]
+                          Mentor Graphics       [N/A]
+                          Sun Fire              [N/A]
+                          Marvell MV78230       [N/A]
+                          Allwinner Sun4i       [N/A]
+                          DECchip 21272         [N/A]
+DAC
+                          TDA8444               0x40, 0x42, 0x44, 0x46, 0x48, 0x4a, 0x4c, 0x4e
+EEPROM
+                          AT24C{01,02,04,08,16} 0x50 to 0x57
+                          AT24C32, AT24C64      0x50 to 0x57
+                          AT34C02               0x50 to 0x57, 0x30 to 0x37
+                          PCF8582C              0x50 to 0x57
+                          SPD memory            0x50 to 0x57
+GPIO
+                          FM3560                0x37, 0x4e
+                          PCF8574, PCF8574A     0x20 to 0x27, 0x38 to 0x3f
+                          PCA9555               0x20 to 0x27
+                          PCA9556               0x18 to 0x1f
+PIC/Microcontroller
+                          PIC16F818/819         0x29
+Real-Time Clock
+                          DS1307                0x68
+Sensor/Hardware Monitor
+                          ADM1021               0x18 to 0x1a, 0x29 to 0x2b, 0x4c to 0x4e
+                          ADM1021               0x18 to 0x1a, 0x29 to 0x2b, 0x4c to 0x4e
+                          ADM1021A              0x18 to 0x1a, 0x29 to 0x2b, 0x4c to 0x4e
+                          ADM1023               0x18 to 0x1a, 0x29 to 0x2b, 0x4c to 0x4e
+                          ADM1026               0x2c to 0x2f
+                          ADM1031               0x2c to 0x2f
+                          ADM1032               0x4c, 0x4d
+                          ADM9240               0x2c to 0x2f
+                          ADT7462               0x5b, 0x5c
+                          DS1780                0x2c to 0x2f
+                          DS75                  0x48 to 0x4f
+                          G781                  0x4c
+                          LM75                  0x48 to 0x4f
+                          LM75A                 0x48 to 0x4f
+                          LM76                  0x48 to 0x4d
+                          LM77                  0x48 to 0x4b
+                          LM81                  0x2c to 0x2f
+                          LM84                  0x18 to 0x1a, 0x29 to 0x2b, 0x4c to 0x4e
+                          LM87                  0x2c to 0x2f
+                          MAX1617               0x18 to 0x1a, 0x29 to 0x2b, 0x4c to 0x4e
+                          MAX1617A              0x18 to 0x1a, 0x29 to 0x2b, 0x4c to 0x4e
+                          NE1617ADS             0x18 to 0x1a, 0x29 to 0x2b, 0x4c to 0x4e
+Smart Card Interface
+                          TDA8020HL             0x40, 0x42, 0x44, 0x46, 0x48, 0x4a, 0x4c, 0x4e
+*/
 package bus
 
 // based on https://github.com/davecheney/i2c/blob/master/i2c.go
